a82968d2aa24979678e43f40f0e7bf1f51dc83cf
Sarim Siddiqui <sarim.siddiq@gmail.com>
2022-02-15 01:56:09 +0500

    forloop destructuring

---
+++ ES6Destructring.js
@@ -0,0 +1,40 @@
+//Declaration:
+//const [firstName, lastName] = ["Sarim", "Siddiqui"];
+let [firstName, lastName] = ["Sarim", "Siddiqui"]; //Available from ES6
+//var [firstName, lastName] = ["Sarim", "Siddiqui"]; //Available from ES6
+
+console.log(firstName + ' ' + lastName);
+
+//Assignment:
+
+[firstName, lastName] = ["Sarim1", "Siddiqui1"]
+
+console.log(firstName + ' ' + lastName);
+
+var {customerFirstName, customerLastName} = {customerFirstName: "Adeel", customerLastName: "Hussain"};
+
+console.log(customerFirstName + ' ' + customerLastName);
+
+[name, room, weapon] = ["Rusty", "kitchen", "candlestick"];
+
+console.log(name + ' ' + room + ' ' + weapon);
+
+
+const game = {
+    'suspects' : [
+        {
+            name : "Rusty",
+            color: "Orange"
+        },
+        {
+            name : "Miss Scarlet",
+            color: "Red"
+        }
+    ]
+}
+
+game["suspects"].forEach(suspect => {console.log(suspect);});
+// foreach(let suspect = game["suspects"])
+// {
+//     console.log(suspect);
+// }
